ff9431859081d534095e8853767e57a42929412f
mahahahajan <pulkit8.mahajan@gmail.com>
2024-06-04 20:45:44 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const qs = (selector) => document.querySelector(selector);
+const question = qs(".question");
+const gif = qs(".gif");
+const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
+
+const handleYesClick = () => {
+    window.location.href = "fireworks.html";
+};
+
+const handleNoMouseOver = () => {
+  const { width, height } = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - width;
+  const maxY = window.innerHeight - height;
+
+  noBtn.style.left = `${Math.floor(Math.random() * maxX)}px`;
+  noBtn.style.top = `${Math.floor(Math.random() * maxY)}px`;
+};
+
+yesBtn.addEventListener("click", handleYesClick);
+noBtn.addEventListener("mouseover", handleNoMouseOver);
